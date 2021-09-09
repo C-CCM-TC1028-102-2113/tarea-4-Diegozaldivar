578b95eeaa395d76205d79218fd38911467a16ef
Diegozaldivar <89473869+Diegozaldivar@users.noreply.github.com>
2021-09-08 21:30:51 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,18 @@
 
+# Fibonacci
 def main():
+    index = int(input("Enter the index: "))
     #escribe tu código abajo de esta línea
-    pass
-
-if __name__=='__main__':
+    n_ant = 0
+    n_sig = 1
+    c = 1
+    while c < index:
+        suma = n_sig + n_ant
+        n_ant = n_sig
+        n_sig = suma
+        c = c + 1
+    print(suma)
+        
+         
+if __name__ == '__main__':
     main()
